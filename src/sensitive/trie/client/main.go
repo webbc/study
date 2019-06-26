231871e24b163553d9f8d4de8ec7341d0e591066
webbc <406391609@qq.com>
2019-06-26 10:33:11 +0800

    敏感词过滤优化

--- src/sensitive/trie/client/main.go
+++ src/sensitive/trie/client/main.go
@@ -51,7 +51,7 @@ func CreateSensitiveTree() error {
 	return nil
 }
 
-func SensitiveCheck(word string) string {
+func SensitiveTransform(word string) string {
 
 	t := []rune(strings.ToLower(word)) // 转换为unicode数组
 	p1 := tree.GetRoot()               // p1指向树的根节点
@@ -110,6 +110,61 @@ func SensitiveCheck(word string) string {
 	return newWord
 }
 
+func SensitiveCheck(word string) bool {
+	t := []rune(strings.ToLower(word)) // 转换为unicode数组
+	p1 := tree.GetRoot()               // p1指向树的根节点
+	p2 := t[0]                         // p2是过滤内容的第一个字符
+	var p2Index, p3Index int           // p2的位置,p3的位置,默认是过滤内容的第一个字符的位置
+
+	// p3指向最后一个字符，则结束
+	for ; p3Index < len(t); {
+
+		// 遍历从p3开始到结尾的字符串
+		for i := p3Index; i < len(t); i++ {
+			char := t[i]
+
+			if !p1.Contains(p2) {
+
+				// 如果p2不是p1子节点，说明以p3开始的内容不需要屏蔽，p3指向下一个位置，p2指向p3，并将p1重置到root节点，终止循环
+				p2Index = p3Index + 1
+				p3Index = p3Index + 1
+
+				if p2Index < len(t) {
+					p2 = t[p2Index]
+				}
+				p1 = tree.GetRoot()
+
+				break
+			} else {
+
+				// 如果p2是p1的子节点，p1节点移动到p2内容对应的节点上，p2移动到下个位置
+
+				// 获取p1的子节点
+				p1 = p1.GetChildNode(char)
+
+				// 移动P2到下个位置
+				p2Index = p2Index + 1
+				if p2Index < len(t) {
+					p2 = t[p2Index]
+				}
+
+				if !p1.HasChild() {
+
+					return true
+
+					// p1没有子节点，树的某条路径遍历完了，说明字符串中含有敏感词
+					p1 = tree.GetRoot()
+
+					// p3移动到p2的位置
+					p3Index = p2Index
+				}
+			}
+		}
+	}
+
+	return false
+}
+
 func main() {
 
 	e := CreateSensitiveTree()
@@ -117,6 +172,9 @@ func main() {
 		fmt.Println(e)
 	}
 
-	newStr := SensitiveCheck("本土无码公司待办列宁测试GM")
+	newStr := SensitiveTransform("本土无码公司待办列宁测试GM")
 	fmt.Println(newStr)
+
+	result := SensitiveCheck("列宁")
+	fmt.Println(result)
 }
--- src/sensitive/trie/test/sensitive_test.go
+++ src/sensitive/trie/test/sensitive_test.go
@@ -6,14 +6,35 @@ import (
 	"fmt"
 )
 
+// 转换性能测试
+func Benchmark_SensitiveTransform(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		trie.SensitiveTransform("本土无码公司待办列宁测试GM")
+	}
+}
+
+// 构造性能测试
+func Benchmark_Init(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// 加载屏蔽字树
+		e := trie.CreateSensitiveTree()
+		if e != nil {
+			fmt.Println(e)
+		}
+	}
+}
+
+// 检测性能测试
 func Benchmark_SensitiveCheck(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		trie.SensitiveCheck("本土无码公司待办列宁测试GM")
+	}
+}
+
+func init() {
 	// 加载屏蔽字树
 	e := trie.CreateSensitiveTree()
 	if e != nil {
 		fmt.Println(e)
-	}
-
-	for i := 0; i < b.N; i++ {
-		trie.SensitiveCheck("本土无码公司待办列宁测试GM")
 	}
 }
